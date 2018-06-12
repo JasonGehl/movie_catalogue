2bd5df594a2a7ea568e47a16689e2825a6df7dd7
Jason Gehl <jason.r.gehl@gmail.com>
2018-06-12 16:15:28 -0400

    Initial component, controller, for add movie

---
+++ src/AddMovieController.js
@@ -0,0 +1,15 @@
+export default function AddMovieController($scope, $http){
+  const DATABASE_LOCATION = 'http://localhost:3001';
+
+  const onMovieSaveComplete = function(response){
+    //refresh movie list
+  }
+
+  const onError = function(err){
+    $scope.error = err;
+  }
+
+  $scope.saveMovie = function(){
+    $http.post(DATABASE_LOCATION + '/movies').then(onMovieSaveComplete, onError);
+  }
+}
