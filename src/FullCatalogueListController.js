a928b1caa6024dce27885dae2f8b25d1801c2129
Jason Gehl <jason.r.gehl@gmail.com>
2018-06-12 19:27:45 -0400

    Rename movieList into fullCatalogueList component

--- src/FullCatalogueListController.js
+++ src/FullCatalogueListController.js
@@ -1,4 +1,4 @@
-export default function MovieListController($scope, ApiService){
+export default function FullCatalogueListController($scope, ApiService){
   const onMoviesGetComplete = function(movieData){
     $scope.movies = movieData;
   }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import './index.css';
 import angular from 'angular';
 import ApiService from './api/apiService';
-import MovieListController from './MovieListController';
+import FullCatalogueListController from './FullCatalogueListController';
 import AddMovieController from './AddMovieController';
 import MovieSearchController from './MovieSearchController';
 
 angular.module("MovieCatalogueApp", [])
 .factory("ApiService", ['$http', '$log', ApiService])
-.controller("MovieListController", ['$scope', 'ApiService', MovieListController])
+.controller("FullCatalogueListController", ['$scope', 'ApiService', FullCatalogueListController])
 .controller("AddMovieController", ['$scope', 'ApiService', AddMovieController])
 .controller("MovieSearchController", ['$scope', 'ApiService', MovieSearchController])
-.component('movieList', {
-  template: require('./movieList.html'),
-  controller: MovieListController
+.component('fullCatalogueList', {
+  template: require('./fullCatalogueList.html'),
+  controller: FullCatalogueListController
 })
 .component('addMovie', {
   template: require('./addMovie.html'),
